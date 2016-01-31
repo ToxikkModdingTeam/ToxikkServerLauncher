6525caf1450c87789aa0dac739ce3a8abb066d6a
hbeham <horst@beham.biz>
2016-01-31 18:51:50 +0100

    - added steam auto-update
    - added copying files from workshop to toxikk and HTTP redirect folders

--- ToxikkServerLauncher/Launcher.cs
+++ ToxikkServerLauncher/Launcher.cs
@@ -10,11 +10,18 @@ namespace ToxikkServerLauncher
   class Launcher
   {
     private const string ServerSectionPrefix = "DedicatedServer";
+    private string steamcmdExe;
     private string launcherFolder;
     private string toxikkFolder;
     private string configFolder;
+    private string workshopFolder;
+    private string httpFolder;
     private string toxikkExe;
     private IniFile ini;
+    private bool dedicated = true;
+    private bool showCommandLine;
+    private bool pause;
+    private bool skipWorkshopUpdate;
 
     /// <summary>
     /// Maps logical server setting names to real setting names (either ini-file\section\section specifier or a command line option name) as found in the [SimpleNames] section
@@ -24,19 +31,33 @@ namespace ToxikkServerLauncher
     #region Run()
     public void Run(string[] args)
     {
+      launcherFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
       var serverIds = ParseCommandLine(args);
-      if (serverIds == null)
+      ReadServerConfig();
+      if (!InitFolders())
         return;
 
-      ReadServerConfig();
+      if (System.Diagnostics.Process.GetProcessesByName("toxikk").Length >= 1)
+        Console.WriteLine("TOXIKK.exe is already running, skipping workshop updates.");
+      else
+      {
 
+        DownloadWorkshopItems();
+        Console.WriteLine("Copying workshop item contents to TOXIKK and HTTP redirect folders...");
+        CopyWorkshopContent();
+      }
+
+      // prompt for server IDs when none were specified on the command line
       if (serverIds.Count == 0)
       {
+        Console.WriteLine();
         ListConfigurations();
         Console.Write("Start server(s): ");
         serverIds = (Console.ReadLine() ?? "").Split(' ').ToList();
       }
 
+      // generate server config(s) and start the server(s)
       foreach (var id in serverIds)
         RunServerConfiguration(id);
     }
@@ -52,12 +73,27 @@ namespace ToxikkServerLauncher
         if (arg[0] == '/' || arg[0] == '-')
         {
           int idx = arg.IndexOf('=');
-          string key = idx < 0 ? arg : arg.Substring(1, idx-1);
-          string val = idx < 0 ? "" : arg.Substring(idx + 1);
+          string key = idx < 0 ? arg.Substring(1) : arg.Substring(1, idx-1);
+          string val = idx < 0 ? null : arg.Substring(idx + 1);
           switch (key.ToLower())
           {
             case "toxikkdir":
-              toxikkFolder = val;
+              this.toxikkFolder = val;
+              break;
+            case "workshopdir":
+              this.workshopFolder = val;
+              break;
+            case "listen":
+              this.dedicated = false;
+              break;
+            case "showcommand":
+              this.showCommandLine = true;
+              break;
+            case "pause":
+              this.pause = true;
+              break;
+            case "skipupdate":
+              this.skipWorkshopUpdate = true;
               break;
           }
         }
@@ -65,17 +101,6 @@ namespace ToxikkServerLauncher
           serverIds.Add(arg);
       }
 
-      launcherFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\";
-      if (toxikkFolder == null)
-        toxikkFolder = launcherFolder + @"..";
-      toxikkExe = toxikkFolder + @"\Binaries\Win32\TOXIKK.exe";
-      configFolder = toxikkFolder + @"\UDKGame\Config\";
-
-      if (!File.Exists(toxikkExe))
-      {
-        Console.Error.WriteLine(@"Either copy this program to SteamApps\Common\TOXIKK\TOXIKKServers or use /toxikkdir=...");
-        return null;
-      }
       return serverIds;
     }
 
@@ -84,7 +109,11 @@ namespace ToxikkServerLauncher
     #region ReadServerConfig()
     private void ReadServerConfig()
     {
-      var configFile = launcherFolder + "\\ServerConfig.ini";
+      var myConfigFile = Path.Combine(launcherFolder, "MyServerConfig.ini");
+      var configFile = Path.Combine(launcherFolder, "ServerConfig.ini");
+      if (!File.Exists(myConfigFile))
+        File.Move(configFile, myConfigFile);
+      configFile = myConfigFile;
       ini = new IniFile(configFile);
 
       // import old server config file format if necessary
@@ -99,13 +128,76 @@ namespace ToxikkServerLauncher
       var section = ini.GetSection("SimpleNames");
       foreach (var key in section.Keys)
         keyMapping[key] = section.GetString(key) ?? "";
+
+      // process launcher config 
+      section = ini.GetSection("ServerLauncher");
+      if (section != null)
+      {
+        var toxikkDir = section.GetString("ToxikkDir");
+        if (toxikkDir != null && this.toxikkFolder == null)
+        {
+          var path = Path.Combine(toxikkDir, @"Binaries\win32\TOXIKK.exe");
+          if (File.Exists(path))
+            this.toxikkFolder = toxikkDir;
+        }
+
+        var workshopDir = section.GetString("WorkshopDir");
+        if (workshopDir != null && this.workshopFolder == null && Directory.Exists(workshopDir))
+          this.workshopFolder = workshopDir;
+
+        this.httpFolder = section.GetString("HttpRedirectDir");
+
+        var steamcmdDir = section.GetString("SteamcmdDir");
+        if (steamcmdDir != null)
+        {
+          var exe = Path.Combine(steamcmdDir, "steamcmd.exe");
+          if (File.Exists(exe))
+            this.steamcmdExe = exe;
+        }
+      }
     }
+    #endregion
+
+    #region InitFolders()
+    private bool InitFolders()
+    {
+      toxikkFolder = toxikkFolder?.TrimEnd('\\', '/') ?? Path.Combine(launcherFolder,  "..");
+      toxikkExe = Path.Combine(toxikkFolder, @"Binaries\win32\TOXIKK.exe");
+      configFolder = Path.Combine(toxikkFolder, @"UDKGame\Config");
+
+      if (workshopFolder == null)
+      {
+        // ReSharper disable PossibleNullReferenceException
+        // ReSharper disable AssignNullToNotNullAttribute
+        workshopFolder = Path.GetDirectoryName(toxikkFolder);
+        while (Path.GetFileName(workshopFolder).ToLower() != "steamapps")
+          workshopFolder = Path.GetDirectoryName(workshopFolder);
+        workshopFolder = Path.Combine(workshopFolder, @"workshop\content\324810\");
+        // ReSharper restore PossibleNullReferenceException
+        // ReSharper restore AssignNullToNotNullAttribute
+      }
+
+      if (httpFolder != null && !Directory.Exists(httpFolder))
+        Directory.CreateDirectory(httpFolder);
+
+      if (!File.Exists(toxikkExe))
+      {
+        Console.Error.WriteLine("Could not find TOXIKK.exe.\n" +
+          @"Either copy this program to SteamApps\Common\TOXIKK\TOXIKKServers," +
+          "\nset ToxikkDir in ServerConfig.ini/[ServerLauncher]\n" +
+          "\nor use the /toxikkdir=... command line parameter");
+        return false;
+      }
+
+      return true;
+    }
+
     #endregion
 
     #region ConvertLegacyServerConfigListIni()
     private void ConvertLegacyServerConfigListIni()
     {
-      var oldConfigFile = toxikkFolder + @"\TOXIKKServers\TOXIKKServerLauncher\ServerConfigList.ini";
+      var oldConfigFile = Path.Combine(toxikkFolder, @"TOXIKKServers\TOXIKKServerLauncher\ServerConfigList.ini");
       if (!File.Exists(oldConfigFile))
         return;
 
@@ -149,7 +241,7 @@ namespace ToxikkServerLauncher
         sb.AppendLine($"MaxSkillClass={fields[12]}");
         sb.AppendLine($"Mutators={fields[13]}");
 
-        File.AppendAllText(launcherFolder + "ServerConfig.ini", sb.ToString());
+        File.AppendAllText(Path.Combine(launcherFolder, "MyServerConfig.ini"), sb.ToString());
       }
     }
 
@@ -158,6 +250,7 @@ namespace ToxikkServerLauncher
       if (mode == "BL") return "Cruzade.CRZBloodlust";
       if (mode == "SA") return "Cruzade.CRZTeamGame";
       if (mode == "CC") return "Cruzade.CRZCellCapture";
+      if (mode == "AD") return "Cruzade.CRZAreaDomination";
       return mode;
     }
     #endregion
@@ -189,49 +282,75 @@ namespace ToxikkServerLauncher
       }
 
       Console.WriteLine("Starting " + (section.GetString("ServerName") ?? sectionName));
-      var options = GenerateConfig(section);
-      LaunchServer(sectionName, options);
+      string map, options;
+      if (GenerateConfig(section, out map, out options))
+        LaunchServer(map, options, sectionName);
     }
 
     #endregion
 
     #region GenerateConfig()
-    private string GenerateConfig(IniFile.Section section)
+    private bool GenerateConfig(IniFile.Section section, out string map, out string options)
     {
-      var configSubDir = configFolder + section.Name;
-      Directory.CreateDirectory(configSubDir);
+      string targetConfigFolder;
+      if (this.dedicated)
+      {
+        targetConfigFolder = configFolder + section.Name;
+        Directory.CreateDirectory(targetConfigFolder);
 
-      // copy all Default*.ini files
-      foreach (var file in Directory.GetFiles(configFolder, "Default*.ini"))
-        File.Copy(file, configSubDir + "\\" + Path.GetFileName(file), true);
+        // copy all Default*.ini files
+        foreach (var file in Directory.GetFiles(configFolder, "Default*.ini"))
+          File.Copy(file, Path.Combine(targetConfigFolder, Path.GetFileName(file)??""), true);
 
-      // copy UDK*.ini where there is no matching Default*.ini
-      // (sometimes UDK* files are accessed before they have been generated from Default* files)
-      foreach (var file in Directory.GetFiles(configFolder, "UDK*.ini"))
+        // copy UDK*.ini where there is no matching Default*.ini
+        // (sometimes UDK* files are accessed before they have been generated from Default* files)
+        foreach (var file in Directory.GetFiles(configFolder, "UDK*.ini"))
+        {
+          var fileName = Path.GetFileName(file) ?? "";
+          var defaultFile = Path.Combine(Path.GetDirectoryName(file)??"", "Default" + fileName.Substring(3));
+          if (!File.Exists(defaultFile))
+            File.Copy(file, Path.Combine(targetConfigFolder, fileName), true);
+        }
+      }
+      else
+        targetConfigFolder = this.configFolder.TrimEnd('\\', '/');
+
+      // copy all UDK*.ini files from the ServerLauncher folder
+      foreach (var file in Directory.GetFiles(launcherFolder, "UDK*.ini"))
       {
         var fileName = Path.GetFileName(file) ?? "";
-        var defaultFile = Path.GetDirectoryName(file) + "\\Default" + fileName.Substring(3);
-        if (!File.Exists(defaultFile))
-          File.Copy(file, configSubDir + "\\" + fileName, true);
+        File.Copy(file, Path.Combine(targetConfigFolder, fileName), true);
       }
 
       var destIniCache = new Dictionary<string, IniFile>();
-      var options = new StringBuilder();
-      options.Append("?dedicated=true");
+      var optionDict = new SortedDictionary<string,string>(StringComparer.InvariantCultureIgnoreCase);
 
-      // recursive processing of a section and it's @Import sections
-      ProcessConfigSection(section.Name, section, destIniCache, options);
+      // recursive processing of a section and its @Import sections
+      ProcessConfigSection(targetConfigFolder, section, destIniCache, optionDict);
+
+      // build URL with map and options
+      if (!optionDict.TryGetValue("map", out map))
+      {
+        Console.Error.WriteLine("ERROR: No map specified");
+        options = null;
+        return false;
+      }
+      optionDict.Remove("map");
+      var sbOptions = new StringBuilder();
+      foreach (var entry in optionDict)
+        sbOptions.Append("?").Append(entry.Key).Append("=").Append(entry.Value.Replace(' ', '_'));
 
       // save the ini files
       foreach (var destIni in destIniCache.Values)
         destIni.Save();
 
-      return options.ToString();
+      options = sbOptions.ToString();
+      return true;
     }
     #endregion
 
     #region ProcessConfigSection()
-    private void ProcessConfigSection(string sectionName, IniFile.Section section, Dictionary<string, IniFile> destIniCache, StringBuilder options)
+    private void ProcessConfigSection(string targetConfigFolder, IniFile.Section section, Dictionary<string, IniFile> destIniCache, SortedDictionary<string,string> options)
     {
       foreach (var unmappedKey in section.Keys)
       {
@@ -243,8 +362,9 @@ namespace ToxikkServerLauncher
         var configMapping = mappedKey.Split('\\');
         if (configMapping.Length == 3)
         {
+          // process a "filename\section\key=value" entry
           IniFile destIni;
-          string destIniPath = configFolder + sectionName + "\\" + configMapping[0];
+          string destIniPath = targetConfigFolder + "\\" + configMapping[0];
           if (!destIniCache.TryGetValue(destIniPath, out destIni))
           {
             destIni = new IniFile(destIniPath);
@@ -252,36 +372,165 @@ namespace ToxikkServerLauncher
           }
           var destSec = destIni.GetSection(configMapping[1], true);
           destSec.Set(configMapping[2], value);
-        }
-        else if (mappedKey.ToLower() == "map")
-          options.Insert(0, value);
+        }        
         else if (mappedKey.ToLower() == "@import")
-          ProcessConfigSection(sectionName, ini.GetSection(value), destIniCache, options);
+        {
+          // recursively process settings from another section
+          ProcessConfigSection(targetConfigFolder, ini.GetSection(value), destIniCache, options);
+        }
         else if (mappedKey.ToLower() == "@copyfiles")
         {
+          // copy files specified as "source:dest,source:dest,...". 
+          // source files can be placed in the ServerLauncher folder or the template config folder
           foreach (var fileInfo in value.Split(','))
           {
-            var names=fileInfo.Split('=',':','\\','/');
+            var names = fileInfo.Split('=', ':', '\\', '/'); // various separator chars to prevent exploits with absolute paths
             if (names.Length == 2 && names[0] != "" && names[1] != "")
             {
-              var folder = File.Exists(launcherFolder + names[0]) ? launcherFolder : configFolder;
-              File.Copy(folder + names[0], configFolder + sectionName + "\\" + names[1], true);
+              var folder = File.Exists(Path.Combine(launcherFolder, names[0])) ? launcherFolder : configFolder;
+              var source = Path.Combine(folder, names[0]);
+              if (File.Exists(source))
+                File.Copy(Path.Combine(folder, names[0]), Path.Combine(targetConfigFolder, names[1]), true);
+              else
+                Console.Error.WriteLine("WARNING: @copyfile source not found: " + names[0]);
             }
           }
         }
-        else //if (value.Trim() != "")
-          options.Append("?").Append(mappedKey).Append("=").Append(value.Replace(' ', '_'));
+        else
+        {
+          // everything else is appended as ?key=value to the UE3 URL
+          options[mappedKey] = value;
+        }
       }
     }
 
     #endregion
 
-    #region LaunchServer()
-    private void LaunchServer(string sectionName, string options)
+    #region DownloadWorkshopItems()
+    private void DownloadWorkshopItems()
     {
-      var args = "server " + options + "?steamsockets -CONFIGSUBDIR=" + sectionName + " -nohomedir -unattended";
-      //Console.WriteLine(toxikkExe + " " + args);
+      if (this.skipWorkshopUpdate)
+        return;
+
+      if (this.steamcmdExe == null)
+      {
+        Console.WriteLine("Steamcmd not configured, skipping workshop updates.");
+        return;
+      }
+
+      var sec = ini.GetSection("SteamWorkshop");
+      if (sec == null)
+        return;
+
+      var items = sec.GetAll("Item");
+      if (items.Count == 0)
+        return;
+
+      var user = sec.GetString("User");
+      var pass = sec.GetString("Password");
+      if (string.IsNullOrWhiteSpace(user) || string.IsNullOrWhiteSpace(pass))
+      {
+        Console.WriteLine("User/Password not configured in [SteamWorkshop], skipping workshop updates.");
+        return;
+      }
+
+      var sb = new StringBuilder();
+      sb.Append("+login ").Append(user).Append(" ").Append(pass).Append(" +app_update 324810");
+      foreach (var item in items)
+        sb.Append(" +workshop_download_item 324810 ").Append(item);
+      sb.Append(" +quit");
+
+      Console.WriteLine("Updating TOXIKK and Steam Workshop Items...\n");
+      var psi = new System.Diagnostics.ProcessStartInfo(this.steamcmdExe, sb.ToString());
+      psi.UseShellExecute = false;
+      var proc = System.Diagnostics.Process.Start(psi);
+      proc?.WaitForExit();
+      Console.WriteLine("\nSteam update complete.\n");
+    }
+    #endregion
+
+    #region CopyWorkshopContent()
+    private bool CopyWorkshopContent()
+    {
+      if (!Directory.Exists(workshopFolder))
+        return true;
+
+      var toxikkDir = Path.Combine(this.toxikkFolder, @"UDKGame\Workshop");
+      try
+      {
+        foreach (var itemPath in Directory.GetDirectories(workshopFolder))
+          CopyFolder(itemPath, toxikkDir);
+        return true;
+      }
+      catch (IOException ex)
+      {
+        Console.Error.WriteLine("Failed to copy workshop item: " + ex.Message);
+        return false;
+      }
+    }
+    #endregion
+
+    #region CopyFolder()
+    private void CopyFolder(string sourceDir, string targetDir)
+    {
+      if (!Directory.Exists(targetDir))
+        Directory.CreateDirectory(targetDir);
+
+      // ReSharper disable AssignNullToNotNullAttribute
+      foreach (var file in Directory.GetFiles(sourceDir))
+      {
+        // copy files to toxikk/udkgame/workshop/....
+        var target = Path.Combine(targetDir, Path.GetFileName(file));
+        if (File.GetLastWriteTimeUtc(file) != File.GetLastWriteTimeUtc(target) || new FileInfo(file).Length != new FileInfo(target).Length)
+          File.Copy(file, target, true);
+
+        // copy files to HTTP redirect
+        if (httpFolder != null)
+        {
+          target = Path.Combine(httpFolder, Path.GetFileName(file));
+          if (File.GetLastWriteTimeUtc(file) != File.GetLastWriteTimeUtc(target) || new FileInfo(file).Length != new FileInfo(target).Length)
+            File.Copy(file, target, true);
+        }
+      }
+      
+      // recursively process subdirectories
+      foreach (var dir in Directory.GetDirectories(sourceDir))
+        CopyFolder(dir, Path.Combine(targetDir, Path.GetFileName(dir)));
+      // ReSharper restore AssignNullToNotNullAttribute
+    }
+    #endregion
+
+    #region LaunchServer()
+    private void LaunchServer(string map, string options, string sectionName)
+    {
+      string args = "";
+      if (dedicated)
+        args = "server ";
+
+      args += map;
+
+      if (dedicated)
+        args += "?dedicated=true";
+      else
+        args += "?listen=true";
+
+      args += options;
+
+      args += "?steamsockets";
+
+      if (dedicated)
+        args += " -configsubdir=" + sectionName + " -nohomedir -unattended";
+      else
+        args += " -log -nostartupmovies";
+
+      if (this.showCommandLine)
+        Console.WriteLine(toxikkExe + " " + args);
+
+      Environment.CurrentDirectory = Path.GetDirectoryName(toxikkExe) ?? "";
       System.Diagnostics.Process.Start(toxikkExe, args);
+
+      if (this.pause)
+        Console.ReadLine();
     }
     #endregion
 
--- readme.md
+++ readme.md
@@ -2,36 +2,45 @@ ToxikkServerLaucher
 ===
 
 This little tool helps you manage a whole set of different server configurations without the typical copy&paste, version update, ... issues.
-It dynamically generates configuration file folders from template files and then applies the changes for individual servers.
-It also builds the command line with server specific options needed to launch toxikk.exe.
-And it allows you to use frendly settings names instead of the real INI or command line option names.
+Main features:
+- update TOXIKK and workshop items through steamcmd.exe
+- automatically copies downloaded workshop items into TOXIKK and HTTP redirect folders
+- single centralized configuration file used to dynamically generate config folders and files for individual servers.
+- builds the command line with server specific options needed to launch TOXIKK.exe as a dedicated server.
+- allows you to use friendly settings names instead of the real INI or command line option names.
 
 The @Import=<section-name> command allows you to re-use common configuration parts in multiple server configurations.  
 The @CopyFiles=\<from:to,...\> command allows you to copy+rename special configuration files for specific servers (e.g. DefaultMapList.ini with only BL/SA maps).
 
 Setup
 -----
-Put ToxikkServerLauncher.exe and ServerConfig.ini in your SteamApps\\Common\\TOXIKK\\TOXIKKServers directory.  
+See FIRST STEPS inside ServerConfig.ini and edit the necessary settings. The file will be automatically renamed to MyServerConfig.
 The first time you start the .exe it will import your old server settings from TOXIKKServerLauncher\\ServerConfigList.ini into ServerConfig.ini. 
 (You can force a re-import by removing all [DedicatedServer...] sections from ServerConfig.ini.)
 
 Usage
 -----
 When you start the .exe without any command line parameters, you'll get a list of available server configurations. 
-Enter the configuration ID (or multiple separated by spaces) to start it/them.
-You can also specify the configuration IDs on the command line to start the server(s) without user input.
+Enter the configuration ID (or multiple separated by spaces) to start server instances.  
+You can also specify the configuration IDs on the command line to start the server(s) without user input.  
+To auto-download updates from Steam, all TOXIKK.exe processes must be terminated first to unlock files.
+
 
 What it does
 ------------
 The launcher copies SteamApps\\Common\\TOXIKK\\UDKGame\\Config\\Default*.ini to a DedicatedServer... subdirectory, overwriting any existing files.
 It also copies all UDK*.ini files which don't have a corresponding Default*.ini. (Such files are needed for custom mutators to solve a race condition 
 between accessing the UDK* file and generating it from a Default* file.)
-It then applies the INI changes defined in your ServerConfig.ini [DedicatedServer...] sections to the files in the subdirectory.
-All settings in ServerConfig.ini which don't refer to an INI file are used to build the command line options for toxikk.exe.
+Then it applies the INI changes defined in your ServerConfig.ini [DedicatedServer...] sections to the files in the subdirectory.
+All settings in ServerConfig.ini which don't refer to an INI file are used as the command line options for toxikk.exe.
 
 ServerConfig.ini
 ----------------
-There's lots of commentry inside the file to help you build your own config.
+There's lots of comments inside the file to help you build your own config.
+
+The \[ServerLauncher\] section contains some settings for the launcher itself, mostly directory names you need to adjust.
+
+The \[SteamWorkshop\] section contains your login information and the workshop item numbers you want to download/update.
 
 The \[SimpleNames\] section can be used to define friendlier names to be used inside your other sections instead of the full INI path or command line option name.
 
@@ -45,4 +54,3 @@ Other sections have no direct meaning, but can be used with @Import=\<section-na
 Compiling from source
 ---------------------
 I used VS2015 and .NET 4.0.  
-You can use the command line switch /toxikkdir=\<path-to-steamapps-common-toxikk\> in your project's debug settings to tell the program where to find toxikk.
